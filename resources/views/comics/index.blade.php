1b481b03f60e941619f91b2eae2386eeeda53318
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-23 15:55:31 +0200

    added base structure for a card

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,7 +3,23 @@
 @section("page-title", "Comics")
 
 @section("main-content")
+<section class="container">
+  <div class="row justify-content-center">
+    <div class="col-3">
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">
+                    Card title
+                </h5>
+                <p class="card-text">
 
+                </p>
+            </div>
+        </div>
+    </div>
+  </div>
+</section>
 
 
 @endsection
