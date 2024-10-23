71fa501da5d39e4d9b01121a41ec2c38c341fa24
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-23 16:55:52 +0200

    added icon in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,9 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar
-        </a>
+            <a class="navbar-brand" href="#">
+                <img src="{{ Vite::asset("resources/img/dc-logo.png")}}" alt="logo" width="30" height="24" class="d-inline-block align-text-top">
+            </a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
