79c614d0e530cbd163dd77bb6cd8a405fd9e354e
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-23 17:36:26 +0200

    added basic scheme to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,123 @@
 <footer>
-    <h2>
-        footer
-    </h2>
+  <div class="container">
+    <div class="footer-top d-flex">
+        <div class="footer-top-left">
+            <div class="row">
+                <div class="col">
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Character</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+                        <li>
+                            <a href="#">Games</a>
+                        </li>
+                        <li>
+                            <a href="#">Videos</a>
+                        </li>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Shop DC</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop DC Collectibles</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>DC</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Term Of Use</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy(new)</a>
+                        </li>
+                        <li>
+                            <a href="#">Ad Choices</a>
+                        </li>
+                        <li>
+                            <a href="#">Advertising</a>
+                        </li>
+                        <li>
+                            <a href="#">Jobs</a>
+                        </li>
+                        <li>
+                            <a href="#">Subscriptions</a>
+                        </li>
+                        <li>
+                            <a href="#">Talent Workshops</a>
+                        </li>
+                        <li>
+                            <a href="#">CPSC Certificates</a>
+                        </li>
+                        <li>
+                            <a href="#">Ratings</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop Help</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>SITES</h4>
+                    <ul>
+                        <li>
+                            <a href="#">DC</a>
+                        </li>
+                        <li>
+                            <a href="#">MAD Magazine</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Kids</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Universe</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Power Visa</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-top-right">
+            <img src="{{ Vite::asset("resources/img/dc-logo-bg.png")}}" alt="logo-big">
+        </div>
+    </div>
+    <div class="footer-bottom bg-body d-flex justify-content-between">
+        <div class="sign-up">
+            <button type="button" class="btn btn-secondary border-2 border-primary text-white">SIGN-UP NOW!</button>
+        </div>
+        <div class="socials d-flex align-items-center gap-3">
+            <h4 class="text-primary">FOLLOW US</h4>
+            <div class="icons-social">
+                <img src="{{ Vite::asset("resources/img/footer-facebook.png")}}" alt="facebook-icon">
+                <img src="{{ Vite::asset("resources/img/footer-twitter.png")}}" alt="twitter-icon">
+                <img src="{{ Vite::asset("resources/img/footer-youtube.png")}}" alt="youtube-icon">
+                <img src="{{ Vite::asset("resources/img/footer-pinterest.png")}}" alt="pinterest-icon">
+                <img src="{{ Vite::asset("resources/img/footer-periscope.png")}}" alt="periscope-icon">
+            </div>
+        </div>
+    </div>
+  </div>
 </footer>
