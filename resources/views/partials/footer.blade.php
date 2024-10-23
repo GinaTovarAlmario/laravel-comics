a755fc642e87fe49279e53c6d6eb08b1897573f1
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-23 18:51:20 +0200

    added style to footer top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,44 +1,43 @@
-<footer>
-  <div class="container">
-    <div class="footer-top d-flex">
-        <div class="footer-top-left">
+<footer class="container-fluid">
+    <div class="row footer-top d-flex flex-nowrap">
+        <div class="footer-top-left py-4">
             <div class="row">
                 <div class="col">
-                    <h4>DC COMICS</h4>
-                    <ul>
-                        <li>
-                            <a href="#">Character</a>
-                        </li>
-                        <li>
-                            <a href="#">Comics</a>
-                        </li>
-                        <li>
-                            <a href="#">Movies</a>
-                        </li>
-                        <li>
-                            <a href="#">TV</a>
-                        </li>
-                        <li>
-                            <a href="#">Games</a>
-                        </li>
-                        <li>
-                            <a href="#">Videos</a>
-                        </li>
-                        <li>
-                            <a href="#">News</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col">
-                    <h4>SHOP</h4>
-                    <ul>
-                        <li>
-                            <a href="#">Shop DC</a>
-                        </li>
-                        <li>
-                            <a href="#">Shop DC Collectibles</a>
-                        </li>
-                    </ul>
+                    <div class="inside-wrapper">
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            <li>
+                                <a href="#">Character</a>
+                            </li>
+                            <li>
+                                <a href="#">Comics</a>
+                            </li>
+                            <li>
+                                <a href="#">Movies</a>
+                            </li>
+                            <li>
+                                <a href="#">TV</a>
+                            </li>
+                            <li>
+                                <a href="#">Games</a>
+                            </li>
+                            <li>
+                                <a href="#">Videos</a>
+                            </li>
+                            <li>
+                                <a href="#">News</a>
+                            </li>
+                        </ul>
+                        <h4 class="mt-3">SHOP</h4>
+                        <ul>
+                            <li>
+                                <a href="#">Shop DC</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop DC Collectibles</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="col">
                     <h4>DC</h4>
@@ -101,23 +100,23 @@
             </div>
         </div>
         <div class="footer-top-right">
-            <img src="{{ Vite::asset("resources/img/dc-logo-bg.png")}}" alt="logo-big">
+            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="logo-big">
         </div>
     </div>
     <div class="footer-bottom bg-body d-flex justify-content-between">
         <div class="sign-up">
-            <button type="button" class="btn btn-secondary border-2 border-primary text-white">SIGN-UP NOW!</button>
+            <button type="button" class="btn btn-secondary border-2 border-primary text-white">SIGN-UP
+                NOW!</button>
         </div>
         <div class="socials d-flex align-items-center gap-3">
             <h4 class="text-primary">FOLLOW US</h4>
             <div class="icons-social">
-                <img src="{{ Vite::asset("resources/img/footer-facebook.png")}}" alt="facebook-icon">
-                <img src="{{ Vite::asset("resources/img/footer-twitter.png")}}" alt="twitter-icon">
-                <img src="{{ Vite::asset("resources/img/footer-youtube.png")}}" alt="youtube-icon">
-                <img src="{{ Vite::asset("resources/img/footer-pinterest.png")}}" alt="pinterest-icon">
-                <img src="{{ Vite::asset("resources/img/footer-periscope.png")}}" alt="periscope-icon">
+                <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="facebook-icon">
+                <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="twitter-icon">
+                <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="youtube-icon">
+                <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="pinterest-icon">
+                <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="periscope-icon">
             </div>
         </div>
     </div>
-  </div>
 </footer>
