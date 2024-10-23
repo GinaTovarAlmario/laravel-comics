acbd980cfc50b411423fdaffc1ba7065f8d6b38b
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-23 19:39:02 +0200

    fixed style comics in page

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,13 +3,13 @@
 @section("page-title", "Comics")
 
 @section("main-content")
-<section id="comics" class="bg-black pt-4">
+<section id="comics" class="bg-dark pt-4">
     <div class="container">
         <div class="row justify-content-center">
             @foreach ($comics as $comic )
                 <div class="col-2 mb-3">
-                    <div class="card h-100">
-                        <img src="{{ $comic["thumb"] }}" class="card-img-top" alt="{{ $comic["title"] }}">
+                    <div class="card box text-white border border-0 ">
+                            <img src="{{ $comic["thumb"] }}" class="card-img-top box-img rounded-0" alt="{{ $comic["title"] }}">
                         <div class="card-body">
                             <h5 class="card-title">
                                 {{ $comic["title"] }}
