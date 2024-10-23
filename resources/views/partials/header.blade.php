317f36dd028a03b6e0928fbd063e995d908c6724
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-23 16:50:40 +0200

    added first scheme navbar header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,44 @@
 <header>
-    <h1>
-        Header
-    </h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+        </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Characters</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Comics</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Movies</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Tv</a>
+                </li> <li class="nav-item ">
+                    <a class="nav-link" href="#">Games</a>
+                </li> <li class="nav-item ">
+                    <a class="nav-link" href="#">Collectibles</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Videos</a>
+                </li> <li class="nav-item ">
+                    <a class="nav-link" href="#">Fans</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link dropdown-toggle" href="#">Shop</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
 </header>
